327d4d99a067e1c0b944afa10d91497d8ab2d70f
aleksardo <sahar.bo@mail.ru>
2025-08-27 07:30:22 +0300

    добавил комнент в дефун get-int

--- files/mln.clsp.lisp
+++ files/mln.clsp.lisp
@@ -1,9 +1,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun get-int()
-
+"для ввода целых чисел"
   (loop
 (format t "только целое число")
 (setq intval (read))
     (if (typep intval 'integer)
         (return intval)
-        )))                          
+        )))                                          
